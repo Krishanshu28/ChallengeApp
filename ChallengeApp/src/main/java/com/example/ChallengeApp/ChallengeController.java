ab0b4f29233bf368871371b4cb815af2fa7de5ed
Krishanshu <krishnasrivastava2811@gmail.com>
2025-05-17 22:21:44 +0530

    App Completed

---
+++ ChallengeApp/src/main/java/com/example/ChallengeApp/ChallengeController.java
@@ -0,0 +1,69 @@
+package com.example.ChallengeApp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/challenges")
+public class ChallengeController {
+
+
+    private ChallengeService challengeService;
+
+    public ChallengeController(ChallengeService challengeService)
+    {
+        this.challengeService = challengeService;
+    }
+
+
+    //this method tells spring that any request come with this(/challenges) endpoint execute this method
+    @GetMapping
+    public ResponseEntity<List<Challenge>> getAllChallenges(){
+        return new ResponseEntity<>(challengeService.getAllChallenges(), HttpStatus.OK);
+    }
+
+
+    //This method get this parameter from request body. This post request has some additional info with it i.e, request body
+    @PostMapping
+    public ResponseEntity<String> addChallenge(@RequestBody Challenge challenge)
+    {
+        boolean isChallengeAdded =  challengeService.addChallenge(challenge);
+        if(isChallengeAdded)
+            return new ResponseEntity<>("Challenge added successfully", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Challenge not added successfully", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/{month}")
+    public ResponseEntity<Challenge> getAChallenge(@PathVariable String month){
+        Challenge challenge = challengeService.getChallenge(month);
+        if(challenge != null)
+            return new ResponseEntity<>(challenge, HttpStatus.OK);
+        else
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateChallenge(@PathVariable Long id,@RequestBody Challenge updatedChallenge)
+    {
+        boolean isChallengeUpdated = challengeService.updateChallenge(id, updatedChallenge);
+        if(isChallengeUpdated)
+            return new ResponseEntity<>("Challenge updated successfully", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Challenge not updated successfully", HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteChallenge(@PathVariable Long id)
+    {
+        boolean isChallengeDeleted = challengeService.deleteChallenge(id);
+        if(isChallengeDeleted)
+            return new ResponseEntity<>("Challenge deleted successfully", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Challenge not deleted successfully", HttpStatus.NOT_FOUND);
+    }
+}
